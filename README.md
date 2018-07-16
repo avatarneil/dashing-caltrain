d5bbc23774eaba776ee94c45764824dc806c68ea
Neil Goldader <avatarneil@gmail.com>
2018-07-16 11:24:27 -0700

    Added env variables, and catching if Caltrain API is down.

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ A simple Caltrain widget for Dashing.io
 ## Installation
 * Generate an API key for Caltrain from 511.org
 * Clone the repo, and then open a terminal in the root of the repo.
-* Add `export CALTRAIN_API_KEY="{Your 511 Api Key}"` to your .bashrc, .bash_profile, .zshrc, etc... (This depends on what shell you use)
+* Add `export CALTRAIN_API_KEY="{Your 511 API Key}"` to your .bashrc, .bash_profile, .zshrc, etc... (This depends on what shell you use)
+* Add `export NORTH_BOUND_CODE="{Northbound Stop Code}"` and `export SOUTH_BOUND_CODE="{Southbound Stop Code}"` to your .bashrc, .bash_profile, .zshrc, etc... (This depends on what shell you use). These stop codes (Northbound Stop Code and Southbound Stop Code) can be found by hitting http://api.511.org/transit/StopMonitoring?api_key={Your 511 API Key} &agency=CT, and then searching for the name of your Caltrain stop eg. Millbrae
 * `mkdir DASHBOARD_PATH/widgets/caltrain` where DASHBOARD_PATH is the path to your dashboard you wish to install the widget on.
 * `cp -a widgets/caltrain DASHBOARD_PATH/widgets/caltrain` 
 * `cp -a jobs/caltrain.rb DASHBOARD_PATH/jobs`
--- jobs/caltrain.rb
+++ jobs/caltrain.rb
@@ -2,21 +2,31 @@ require 'net/http'
 require 'json'
 require 'time'
 
-$north_bound_code = '70061'
-$south_bound_code = '70062'
+$north_bound_code = ENV["NORTH_BOUND_CODE"]
+$south_bound_code = ENV["SOUTH_BOUND_CODE"]
 
 def north_fetch() 
     uri = URI('http://api.511.org/transit/StopMonitoring?api_key=' + ENV["CALTRAIN_API_KEY"] + '&agency=CT&format=JSON&stopCode=' + $north_bound_code)
     res = Net::HTTP.get(uri)
     res = JSON.parse!(res[3..res.length])
-    res["ServiceDelivery"]["StopMonitoringDelivery"]["MonitoredStopVisit"][0]["MonitoredVehicleJourney"]["MonitoredCall"]
+    if res["ServiceDelivery"]["StopMonitoringDelivery"]["MonitoredStopVisit"][0] == nil
+        # fetch scheduled time
+        "Caltrain API Down"
+    else
+        res["ServiceDelivery"]["StopMonitoringDelivery"]["MonitoredStopVisit"][0]["MonitoredVehicleJourney"]["MonitoredCall"]
+    end
 end
 
 def south_fetch() 
     uri = URI('http://api.511.org/transit/StopMonitoring?api_key=' + ENV["CALTRAIN_API_KEY"] + '&agency=CT&format=JSON&stopCode=' + $south_bound_code)
     res = Net::HTTP.get(uri)
     res = JSON.parse!(res[3..res.length])
-    res["ServiceDelivery"]["StopMonitoringDelivery"]["MonitoredStopVisit"][0]["MonitoredVehicleJourney"]["MonitoredCall"]
+    if res["ServiceDelivery"]["StopMonitoringDelivery"]["MonitoredStopVisit"][0] == nil
+        # fetch scheduled time
+        "Caltrain API Down"
+    else
+        res["ServiceDelivery"]["StopMonitoringDelivery"]["MonitoredStopVisit"][0]["MonitoredVehicleJourney"]["MonitoredCall"]
+    end
 end
 
 SCHEDULER.every '1m' do
@@ -24,10 +34,16 @@ SCHEDULER.every '1m' do
         puts ENV["CALTRAIN_API_KEY"]
         north_bound = north_fetch()
         south_bound = south_fetch()
-        north_bound_time = Time.parse(north_bound["AimedArrivalTime"])
-        south_bound_time = Time.parse(south_bound["AimedArrivalTime"])
-        json_north = {label: "Northbound", value: north_bound_time.getLocal(-8000)}
-        json_south = {label: "Southbound", value: south_bound_time.getLocal(-8000)}
-        send_event("caltrain", { items: [json_north, json_south] })
+        if north_bound  == "Caltrain API Down"|| south_bound == "Caltrain API Down"
+            json_north = {label: "Northbound", value: "Caltrain API Down"}
+            json_south = {label: "Southbound", value: "Caltrain API Down"}
+            send_event("caltrain", items: [json_north, json_south])
+        else
+            north_bound_time = Time.parse(north_bound["AimedArrivalTime"])
+            south_bound_time = Time.parse(south_bound["AimedArrivalTime"])
+            json_north = {label: "Northbound", value: north_bound_time.getlocal("-07:00")}
+            json_south = {label: "Southbound", value: south_bound_time.getlocal("-07:00")}
+            send_event("caltrain", { items: [json_north, json_south] })
+        end
     end
 end
